4c34bf4c460f7be5aaaaa7c05cc9fdbfea6b990a
imoralessirgo <imoralessirgo@outlook.com>
2020-04-30 00:41:39 -0400

    add compare counter

--- src/NaiveSearch.java
+++ src/NaiveSearch.java
@@ -7,18 +7,24 @@
 
 public class NaiveSearch {
 
+    static int numComp;
+
     public static void search(String txt, String pat)
     {
+        numComp = 0;
         int M = pat.length();
         int N = txt.length();
 
         for (int i = 0; i <= N - M; i++) {
             int j;
-            for (j = 0; j < M; j++)
-                if (txt.charAt(i + j) != pat.charAt(j))
+            for (j = 0; j < M; j++) {
+                numComp++;
+                if (txt.charAt(i + j) != pat.charAt(j)) {
                     break;
-            if (j == M)
-                System.out.println("Pattern found at index " + i);
+                }
+                if (j == M)
+                    System.out.println("Pattern found at index " + i);
+            }
         }
     }
 
